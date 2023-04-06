24f70cbbcf780513f312471b565dd1acfa6771f4
Clayton Wilson <eclawils@amazon.com>
2023-04-06 01:13:53 -0500

    aa-tweaks: Fix AA 'Show Auto-Assign Suggestions' button

---
+++ aa-tweaks.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         AA-Tweaks
+// @namespace    mailto:eclawils@amazon.com
+// @version      0.1
+// @description  Fixes the 'Show Auto-Assign Suggestions' button on the RTW 1.0 Auto Assign page
+// @author       Clayton Wilson
+// @match        https://routingtools-na.amazon.com/modules/modules.jsp*
+// @icon         none
+// @grant        GM_addStyle
+// @run-at   document-end
+// ==/UserScript==
+
+(() => {
+  GM_addStyle(`
+  #autoAssignPageId > div:nth-child(7) > div:nth-child(2) > table > tbody > tr > td:nth-child(1) > fieldset > div:nth-child(5) {
+    margin-bottom: 50px !important;
+  }
+  `);
+})();
