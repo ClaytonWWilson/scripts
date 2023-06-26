dc584b71c969355960f485c50a46358b9aa8337d
Clayton Wilson <eclawils@amazon.com>
2023-06-26 00:47:38 -0500

    RTW2 tweaks quickly copy station info for replan

---
+++ tamper-monkey/rtw2-tweaks.ts
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         RTW-Tweaks
+// @namespace    mailto:eclawils@amazon.com
+// @version      1.0
+// @description  Various tweaks and improvements to RTW 2.0
+// @author       Clayton Wilson
+// @match        https://na.route.planning.last-mile.a2z.com/*
+// @icon         none
+// @grant        GM_addStyle
+// @run-at   document-start
+// ==/UserScript==
+
+(() => {
+  //@ts-ignore
+  GM_addStyle(`
+    p.css-d7vd {
+      user-select: all !important;
+    }
+  `);
+})();
+
+// Easy copy/paste dcap numbers
