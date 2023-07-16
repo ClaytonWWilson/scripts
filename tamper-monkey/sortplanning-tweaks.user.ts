74bdd3dc4e411b49231c8635a8c10ed2a55c0ed6
Clayton Wilson <eclawils@amazon.com>
2023-07-16 00:59:35 -0500

    sortplanning-tweaks Better error handling

--- tamper-monkey/sortplanning-tweaks.user.ts
+++ tamper-monkey/sortplanning-tweaks.user.ts
@@ -17,29 +17,44 @@
   const bodyObserver = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       if (mutation.type === "childList" && mutation.target) {
-        const manifested = (
-          document.querySelector(
-            "#app-layout-content-1 > div > div > div > div.container-display-if-ready.success-container > div > div > div.css-dsf1ob > div > div:nth-child(1) > div > div:nth-child(1) > p.css-3ymn3a"
-          ) as HTMLParagraphElement
-        ).innerText;
+        const manifestedDisplay = document.querySelector(
+          "#app-layout-content-1 > div > div > div > div.container-display-if-ready.success-container > div > div > div.css-dsf1ob > div > div:nth-child(1) > div > div:nth-child(1) > p.css-3ymn3a"
+        ) as HTMLParagraphElement;
 
-        const forcasted = (
-          document.querySelector(
-            "#app-layout-content-1 > div > div > div > div.container-display-if-ready.success-container > div > div > div.css-dsf1ob > div > div:nth-child(1) > div > div:nth-child(2) > p.css-3ymn3a"
-          ) as HTMLParagraphElement
-        ).innerText;
+        const forecastedDisplay = document.querySelector(
+          "#app-layout-content-1 > div > div > div > div.container-display-if-ready.success-container > div > div > div.css-dsf1ob > div > div:nth-child(1) > div > div:nth-child(2) > p.css-3ymn3a"
+        ) as HTMLParagraphElement;
 
-        const bagCount = (
-          document.querySelector(
-            "#app-layout-content-1 > div > div > div > div.container-display-if-ready.success-container > div > div > div.css-dsf1ob > div > div:nth-child(2) > div > div > p.css-3ymn3a"
-          ) as HTMLParagraphElement
-        ).innerText;
+        const bagCountDisplay = document.querySelector(
+          "#app-layout-content-1 > div > div > div > div.container-display-if-ready.success-container > div > div > div.css-dsf1ob > div > div:nth-child(2) > div > div > p.css-3ymn3a"
+        ) as HTMLParagraphElement;
 
         const planInfoSection = document.querySelector(
           "#app-layout-content-1 > div > div > div > div.container-display-if-ready.success-container > div > div > div.css-1iiqi5c"
         );
 
-        if (!manifested || !forcasted || !bagCount || !planInfoSection) return;
+        if (
+          !manifestedDisplay ||
+          !forecastedDisplay ||
+          !bagCountDisplay ||
+          !planInfoSection
+        ) {
+          console.error(
+            "Couldn't find an element on screen. Make sure DCAP has finished running and refresh the page."
+          );
+          return;
+        }
+
+        let manifested = manifestedDisplay.innerText;
+        let forecasted = forecastedDisplay.innerText;
+        let bagCount = bagCountDisplay.innerText;
+
+        if (!manifested || !forecasted || !bagCount) {
+          console.error(
+            "One of the DCAP plan values is empty. Stopping script."
+          );
+          return;
+        }
 
         // Check of copy botton is attached or not
         let copyButton: HTMLAnchorElement | null = document.querySelector(
@@ -55,7 +70,7 @@
 
           copyButton.addEventListener("click", () => {
             navigator.clipboard.writeText(
-              `${manifested}\r\n${forcasted}\r\n${bagCount}`
+              `${manifested}\r\n${forecasted}\r\n${bagCount}`
             );
           });
 
